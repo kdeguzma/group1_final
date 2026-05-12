e19b1a09e27b11b377070d0b4fd13ac93626e2bf
kdeguzma <kdeguzma@umd.edu>
2026-05-11 20:50:53 -0400

    Revised setup.py

--- src/final_project/group1_final/setup.py
+++ src/final_project/group1_final/setup.py
@@ -4,23 +4,40 @@ package_name = "group1_final"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="0.0.1",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/launch", ["launch/search_and_rescue.launch.py"],),
-        ("share/" + package_name + "/config",["config/mission_params.yaml","config/nav2_params.yaml",],),
-        ("share/" + package_name + "/maps",["maps/final_project_map.yaml","maps/final_project_map.pgm",],),
-        ("share/" + package_name + "/rviz",["rviz/nav2.rviz"],),
-        ],
-        
+        (
+            "share/" + package_name + "/launch",
+            ["launch/search_and_rescue.launch.py"],
+        ),
+        (
+            "share/" + package_name + "/config",
+            [
+                "config/mission_params.yaml",
+                "config/nav2_params.yaml",
+            ],
+        ),
+        (
+            "share/" + package_name + "/maps",
+            [
+                "maps/final_project_map.yaml",
+                "maps/final_project_map.pgm",
+            ],
+        ),
+        (
+            "share/" + package_name + "/rviz",
+            ["rviz/nav2.rviz"],
+        ),
+    ],
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="kdeguzma",
     maintainer_email="kdeguzma@umd.edu",
-    description="TODO: Package description",
-    license="TODO: License declaration",
+    description="Final project package for search and rescue",
+    license="Apache-2.0",
     extras_require={
         "test": [
             "pytest",
